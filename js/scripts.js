b8d1ea884a3e77a6652f6e51b4ac65880d46eac1
Lee <reannel@engr-134-20.eastern.ewu.edu>
2014-10-13 13:27:50 -0700

    Version O

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+  $(document).ready(function(){
+    // Target your .container, .wrapper, .post, etc.
+    $("#thing-with-videos").fitVids();
+  });
